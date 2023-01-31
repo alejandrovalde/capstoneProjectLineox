f11bab3977fb4f12d0e6fc3172922b477d7c1b13
Alejandro <garciavaldecasas.alejandro@gmail.com>
2023-01-31 19:21:45 +0100

    hello

--- dataExplorer.py
+++ dataExplorer.py
@@ -1,10 +1,7 @@
 import pandas as pd
 import numpy as np
-from datetime import date
+from datetime import datetime, date
 
-#!!pip install geopy
-#!!pip install folium
-from geopy.geocoders import Nominatim
 import folium
 from folium.plugins import FastMarkerCluster
 
@@ -14,6 +11,9 @@ class dataLineox:
         # Read Data
         self.df = pd.read_csv('data.csv')
         self.df['FCaducidad'] = pd.to_datetime(self.df['FCaducidad'])
+        # Handle Expiration dates
+        self.df['FCaducidad'] = self.df['FCaducidad'].fillna(datetime.now())
+        self.df['Days'] = [int((x.date()-date.today()).days) for x in self.df['FCaducidad']]
 
         # Max and Min Frequencies for slider
         self.maxFreq = float(self.df['Frecuencia'].max())
@@ -31,104 +31,94 @@ class dataLineox:
         
         # Localidad list
         self.localidadList = self.df['Localidad'].sort_values().unique()
-        
+    
+    def calculateKPI(self, lfreq, hfreq, ldays, hdays, com_op, loc_op, owner_op):
+        dfFiltered = self.df.loc[
+            (self.df['Frecuencia'] >= lfreq) 
+            & (self.df['Frecuencia'] <= hfreq) 
+            & (self.df['Days'] >= ldays) 
+            & (self.df['Days'] <= hdays)
+            & (self.df['Comunidad'].isin(com_op))
+            & (self.df['Localidad'].isin(loc_op))
+            & (self.df['Titular'].isin(owner_op))
+        ,:]
+
         #Number of radio links
-        self.rlnbr = len(self.df['Ref'].unique())
-        
+        rlnbr = len(dfFiltered['Ref'].unique())
         #Number of companies
-        self.compnbr = len(self.df['NIF/CIF'].unique())
-        
+        compnbr = len(dfFiltered['NIF/CIF'].unique())
         #Avg, number of radio links per company
-        self.rlperowner = round(self.rlnbr/self.compnbr)
-        
-        # Top 10 companies
-        self.top10 = self.df.groupby(by=['Titular', 'NIF/CIF'])['Ref'].count().sort_values(ascending=False).reset_index().head(10)
-        self.top10 = self.top10.rename(columns={'Ref': 'Radio links number'})
-        self.top10['Radio links share'] = round((self.top10['Radio links number']/self.rlnbr)*100).astype(str) + '%'
-        self.top10.set_index('Titular', inplace=True)
-        
-        # Map
-        self.spain_map = None
-        
-    def createMap(self):
-        # Read Data
-        self.data = pd.read_csv('data.csv')
-        
-        def get_coordinates(city):
-            geolocator = Nominatim(user_agent="spain_explorer")
-            location = geolocator.geocode(city) 
-            latitude = location.latitude
-            longitude = location.longitude
-            return latitude, longitude
+        try: rlperowner = round(rlnbr/compnbr)
+        except: rlperowner = 0 
 
-        # Take a 100 sample of df
-        self.df_sample = self.data.sample(100) 
-        
-        # Create a new dataframe with the coordinates
-        self.df_sample['Latitude'], self.df_sample['Longitude'] = zip(*self.df_sample.apply(lambda x: get_coordinates(x['Provincia_FREQ']), axis=1))
-        self.df_sample['Num_Radiolinks'] = 1 # Create a column 'Num_Radiolinks'
-        # Create df giving the number of radio links per Municipio
-        self.df_agg = self.df_sample.groupby(['Provincia_FREQ'])['Num_Radiolinks'].sum().reset_index() # Aggregate by 'Municipio' and sum the 'Num_Radiolinks' column
-        self.df_agg.rename(columns={'Num_Radiolinks': 'Municipio_Count'}, inplace=True) # Rename the column 'Num_Radiolinks' to the desired name
-        self.df_sample = pd.merge(self.df_sample, self.df_agg, on='Provincia_FREQ') # add the 'Municipio_Count' from df_agg to the df_sample joining by 'Municipio'
+        return rlnbr,compnbr,rlperowner
 
-        #Assign color to each Titular and put it in a column in the df
-        def assign_color(df):
-            color_map = {}
-            unique_titulars = set(self.df_sample['Titular']) # Create a set of unique values for the 'Titular' column
-            for i, titular in enumerate(unique_titulars): # Assign a unique color to each unique value
-                color_map[titular] = 'hsl({}, 50%, 50%)'.format(i * 360 / len(unique_titulars)) # Colors must be very distinct to be easily distinguishable
-            self.df_sample['Color'] = self.df_sample['Titular'].apply(lambda x: color_map[x]) # Add a new column to the dataframe called 'Color'
-            return self.df_sample
-        self.df_sample = assign_color(self.df_sample)
-        
+    def topOwners(self, lfreq, hfreq, ldays, hdays, com_op, loc_op, owner_op):
+        top10 = self.df.loc[
+            (self.df['Frecuencia'] >= lfreq) 
+            & (self.df['Frecuencia'] <= hfreq) 
+            & (self.df['Days'] >= ldays) 
+            & (self.df['Days'] <= hdays)
+            & (self.df['Comunidad'].isin(com_op))
+            & (self.df['Localidad'].isin(loc_op))
+            & (self.df['Titular'].isin(owner_op))
+        ,:]
+        rlnbr = len(top10['Ref'].unique())
+        top10 = top10.groupby(by=['Titular', 'NIF/CIF'])['Ref'].count().sort_values(ascending=False).reset_index().head(10)
+        top10 = top10.rename(columns={'Ref': 'Radio links number'})
+        top10['Radio links share'] = round((top10['Radio links number']/rlnbr)*100).astype(str) + '%'
+        top10 = top10.set_index('Titular')
+
+        return top10
+
+    def createMap(self, lfreq, hfreq, ldays, hdays, com_op, loc_op, owner_op):
+        df = self.df.loc[
+            (self.df['Frecuencia'] >= lfreq) 
+            & (self.df['Frecuencia'] <= hfreq) 
+            & (self.df['Days'] >= ldays) 
+            & (self.df['Days'] <= hdays)
+            & (self.df['Comunidad'].isin(com_op))
+            & (self.df['Localidad'].isin(loc_op))
+            & (self.df['Titular'].isin(owner_op))
+        ,:]
+        if df.shape[0] == 0: return folium.Map(location=[40.416775, -3.703790], zoom_start=6)
+
+        # get coordinates
+        dfProvi = pd.read_csv('provincias.csv')
+        df = df.groupby(['Provincia_FREQ'], as_index=False)['Ref'].count().rename(columns={'Ref': 'Provincia_Count'})
+        df = pd.merge(df, dfProvi, left_on='Provincia_FREQ', right_on='provincias').drop('provincias', axis=1)
+
         # Create a map centered on Spain
-        self.spain_map = folium.Map(location=[40.416775, -3.703790], zoom_start=6)
-        # Create a linear color map
+        spain_map = folium.Map(location=[40.416775, -3.703790], zoom_start=6)
+        #spain_map = folium.Map(location=[40.416775, -3.703790], zoom_start=6, tiles="Stamen Toner", control_scale=False)
+
+        # Create a linear color map for the number of Radiolinks per Municipio, with red for low values and green for high values
         color_map = folium.LinearColormap(
             colors=['red', 'yellow', 'green'],
-            vmin=self.df_sample['Municipio_Count'].min(),
-            vmax=self.df_sample['Municipio_Count'].max())
-        # Create a layer for municipalities with the number of Radiolinks
-        municipalities = folium.FeatureGroup(name='Municipalities')
-        for index, row in self.df_sample.iterrows():
-            folium.Circle(
-                location=[row['Latitude'], row['Longitude']],
-                popup=f"Municipality: {row['Provincia_FREQ']}<br>Number of Radiolinks: {row['Municipio_Count']}",
-                radius=30000,
-                fill=True,
-                stroke=False,
-                color=color_map(row['Municipio_Count']),
-                fill_opacity=0.5
-            ).add_to(municipalities)
-        municipalities.add_to(self.spain_map)
-
+            vmin=df['Provincia_Count'].min(),
+            vmax=df['Provincia_Count'].max()
+        )
         # Add the color map to the map
-        color_map.add_to(self.spain_map)
+        color_map.add_to(spain_map)
 
-        # Create a layer for the top 3 companies per municipality
-        companies = folium.FeatureGroup(name='Top 3 companies per municipality')
-        self.df_companies = self.df_sample.groupby(['Provincia_FREQ', 'Titular']).sum().reset_index()
-        self.df_companies = self.df_companies.sort_values(by=['Provincia_FREQ','Municipio_Count'], ascending=[True,False])
-        self.df_companies = self.df_companies.groupby('Provincia_FREQ').head(3)
-
-        # add the 'Color' column to df_companies joining by 'Titular'
-        self.df_companies = pd.merge(self.df_companies, self.df_sample[['Titular', 'Color']], on='Titular')
-
-        for index, row in self.df_companies.iterrows():
-            folium.CircleMarker(
-                location=[row['Latitude'], row['Longitude']],
-                popup=f"Municipality: {row['Provincia_FREQ']}<br>Company: {row['Titular']}<br>Number of Radiolinks: {row['Municipio_Count']}",
-                radius=5,
+        # Create a layer for municipalities with the number of Radiolinks, and add it to the map
+        provincias = folium.FeatureGroup(name='Provincias')
+        for index, row in df.iterrows():
+            folium.Circle(
+                location=[row['lat'], row['long']],
+                popup=f"Prvincia: {row['Provincia_FREQ']}<br>Number of Radiolinks: {row['Provincia_Count']}",
+                radius=15000,
                 fill=True,
                 stroke=False,
-                fill_color=row['Color'],
-                fillOpacity=0.8
-            ).add_to(companies)
-        companies.add_to(self.spain_map)
+                color=color_map(row['Provincia_Count']),
+                fill_opacity=0.5
+            ).add_to(provincias)
 
-        # Add the layer control
-        folium.LayerControl().add_to(self.spain_map)
-        
-        return self.spain_map
-        
+        provincias.add_to(spain_map)
+
+        marker_cluster = FastMarkerCluster(
+            data=df[['lat', 'long']].values.tolist(),
+            popup=df.apply(lambda x: f"Provincia: {x['Provincia_FREQ']}<br>Number of Radiolinks: {x['Provincia_Count']}", axis=1).tolist()
+        ).add_to(spain_map)
+
+        return spain_map
--- pages/1_🌍_Explorer.py
+++ pages/1_🌍_Explorer.py
@@ -1,9 +1,6 @@
 import streamlit as st
+from streamlit_folium import st_folium
 from dataExplorer import dataLineox
-from datetime import datetime
-from datetime import date
-
-dataLineox = dataLineox()
 
 st.set_page_config(
     page_title="Lineox",
@@ -12,131 +9,69 @@ st.set_page_config(
     initial_sidebar_state="collapsed",
 )
 
-st.header("Data Explorer")
-
-#Cache the dataset csv
+# Cache entire class
 @st.cache
-def convert_df(df):
-    # IMPORTANT: Cache the conversion to prevent computation on every rerun
-    return df.to_csv().encode('utf-8')
-csv = convert_df(dataLineox.df)
+def getClass():
+    return dataLineox()
+dataLineox = getClass()
 
-
-# SECTION 1: COUNTRY-LEVEL METRICS
-
-st.subheader("Country level metrics")
-col1, col2 = st.columns([1,3], gap='small')
-
-with col1:    
-    st.markdown("""<span style='color:#00008B'>**Metrics**</span>""",unsafe_allow_html=True)
-    st.download_button(label="Download raw data as CSV", data=csv, file_name='ESradiolinks.csv', mime='text/csv',)
-    st.metric("Radio links", dataLineox.rlnbr)
-    st.metric("Companies", dataLineox.compnbr)
-    st.metric("Avg. number of radio links per company", dataLineox.rlperowner)
-    
-with col2:
-    st.markdown("""<span style='color:#00008B'>**Top 10 companies**</span>""",unsafe_allow_html=True)
-    st.table(data = dataLineox.top10) 
-
-st.image('banner.png', use_column_width='never')
-
-
-# SECTION 2: FILTERING
-
-st.subheader("Drilling down")
+st.header("Data Explorer")
 
 #Filters
 st.markdown("""<span style='color:#00008B'>**FILTERS**</span>""",unsafe_allow_html=True)
 col1, col2 = st.columns([1,1], gap='medium')
 
 with col1:
-    with st.expander("Comunidad filter"):
+    with st.expander("Comunidad Filter"):
         container = st.container()
-        all = st.checkbox("Select all", key='comunidad')
+        all = st.checkbox("Select all", value = True, key='1')
         if all:
-            com_op = container.multiselect("Filter Comunidad", dataLineox.comunidadList, default=dataLineox.comunidadList)
+            com_op = dataLineox.comunidadList
         else:
-            com_op =  container.multiselect("Filter Comunidad", dataLineox.comunidadList)
+            com_op =  container.multiselect(" ", dataLineox.comunidadList, key='comunidad')
 
-    with st.expander("Localidad filter"):
+    with st.expander("Localidad Filter"):
         container = st.container()
-        all = st.checkbox("Select all", key='localidad')
+        all = st.checkbox("Select all", value = True, key='2')
         if all:
-            loc_op = container.multiselect("Filter Localidad", dataLineox.localidadList, dataLineox.localidadList)
+            loc_op = dataLineox.localidadList
         else:
-            loc_op =  container.multiselect("Filter Localidad", dataLineox.localidadList)
+            loc_op =  container.multiselect(" ", dataLineox.localidadList, key='localidad',)
         
-    with st.expander("Company filter"):
+    with st.expander("Company Filter"):
         container = st.container()
-        all = st.checkbox("Select all", key='company')
+        all = st.checkbox("Select all", value = True, key='3')
         if all:
-            owner_op = container.multiselect("Filter Company", dataLineox.companiesList, dataLineox.companiesList)
+            owner_op = dataLineox.companiesList
         else:
-            owner_op =  container.multiselect("Filter Company", dataLineox.companiesList)
+            owner_op =  container.multiselect(" ", dataLineox.companiesList, key='company')
 
 with col2:
     lfreq, hfreq = st.slider(
     'Range of Frequencies',
-    min_value = dataLineox.minFreq, max_value = dataLineox.maxFreq, value = (dataLineox.minFreq, dataLineox.maxFreq)
+    min_value = dataLineox.minFreq, max_value = dataLineox.maxFreq, value = (dataLineox.minFreq, dataLineox.maxFreq),
     )
-    st.write('You selected between', lfreq, 'and', hfreq)
     
     ldays, hdays = st.slider(
-    'Days until contract expiration',
-    min_value = dataLineox.minDate, max_value = dataLineox.maxDate, value = (dataLineox.minDate, dataLineox.maxDate)
+    'Days until Contract Expiration',
+    min_value = dataLineox.minDate, max_value = dataLineox.maxDate, value = (dataLineox.minDate, dataLineox.maxDate),
     )
-    st.write('You selected between', ldays, 'and', hdays)
-    
 
-# Metrics 
 col1, col2 = st.columns([1,3], gap='small')
 
-table = dataLineox.df.copy(deep=True)
-table['FCaducidad'] = table['FCaducidad'].fillna(datetime.now())
-table['Days'] = [int((x.date()-date.today()).days) for x in table['FCaducidad']]
-table = table[
-        (table['Frecuencia'] >= lfreq) & (table['Frecuencia'] <= hfreq) 
-        & (table['Days'] >= ldays) & (table['Days'] <= hdays)
-        & (table['Comunidad'].isin(com_op))
-        & (table['Localidad'].isin(loc_op))
-        & (table['Titular'].isin(owner_op))
-        ]
-
+# Metrics 
 with col1:
     st.markdown("""<span style='color:#00008B'>**Metrics**</span>""",unsafe_allow_html=True)
-    st.metric("Radio links", len(table['Ref'].unique()))
-    st.metric("Companies", len(table['NIF/CIF'].unique()))
-    st.metric("Avg. number of radio links per company", round(len(table['Ref'].unique())/len(table['NIF/CIF'].unique())))
+    st.metric("Radio links", dataLineox.calculateKPI(lfreq, hfreq, ldays, hdays, com_op, loc_op, owner_op)[0]) 
+    st.metric("Companies", dataLineox.calculateKPI(lfreq, hfreq, ldays, hdays, com_op, loc_op, owner_op)[1])
+    st.metric("Avg. number of radio links per company", dataLineox.calculateKPI(lfreq, hfreq, ldays, hdays, com_op, loc_op, owner_op)[2])
             
-
 #Radio links top 10 table
-top = dataLineox.df.groupby(by=['Titular','NIF/CIF','Comunidad','Localidad','Frecuencia','FCaducidad'])['Ref'].count().sort_values(ascending=False).reset_index()
-top['Days'] = [int((x.date()-date.today()).days) for x in top['FCaducidad']]
-top = top.rename(columns={'Ref': 'Radio links number'})
-
 with col2:
     st.markdown("""<span style='color:#00008B'>**Top 10 radio links groups**</span>""",unsafe_allow_html=True)
+    st.table(data = dataLineox.topOwners(lfreq, hfreq, ldays, hdays, com_op, loc_op, owner_op))
 
-    top = dataLineox.df.groupby(by=['Titular','NIF/CIF','Comunidad','Localidad','Frecuencia','FCaducidad'])['Ref'].count().sort_values(ascending=False).reset_index()
-    top['Days'] = [int((x.date()-date.today()).days) for x in top['FCaducidad']]
-    top = top.rename(columns={'Ref': 'Radio links number'})
+mapdata = st_folium(dataLineox.createMap(lfreq, hfreq, ldays, hdays, com_op, loc_op, owner_op), width=1500, height=500)
 
-    st.table(data = top.loc[
-        (top['Frecuencia'] >= lfreq) & (top['Frecuencia'] <= hfreq) 
-        & (top['Days'] >= ldays) & (top['Days'] <= hdays)
-        & (top['Comunidad'].isin(com_op))
-        & (top['Localidad'].isin(loc_op))
-        & (top['Titular'].isin(owner_op))
-        ,['Titular','NIF/CIF','Frecuencia','Days','Radio links number']].head(10).set_index('Titular')
-        )
-
-#Spain map
-dataLineox.createMap()
-    
-#Full dataset table
-st.markdown("""<span style='color:#00008B'>**Full dataset**</span>""",unsafe_allow_html=True)
-
-with st.expander("Radio links full dataset"):
-    st.table(data = top[['Titular','NIF/CIF','Frecuencia','Days','Radio links number']])
-
-
+st.download_button(label="Download All Raw Data (csv)", data=dataLineox.df.to_csv().encode('utf-8'), file_name='ESradiolinks.csv', mime='text/csv',)
+st.image('banner.png')
